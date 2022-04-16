6cc2c4e6f7263d27a9a67615418c5949a5005a2b
Leandro Afonso da Silva de Almeida <79644610+LeandroASAlmeida@users.noreply.github.com>
2022-04-16 15:35:07 -0300

    Update ex25.py

--- Secao05/ex25.py
+++ Secao05/ex25.py
@@ -1,22 +1,16 @@
-a = float(input('Digite o valor de a (x²): '))---
+a = float(input('Digite o valor A:'))
+b = float(input('Digite o valor B:'))
+c = float(input('Digite o valor C:'))
 
-if (a == 0):#variavel diferente de zero
-    print('Não é uma equação do segundo grau')
-    
+if a == 0:
+    print('Não é uma equação de segundo grau.')
+
+delta = (b ** 2) - 4 * a * c
+if delta < 0:
+    print('Não existe raiz.')
+elif delta == 0:
+    print(delta ** 0.5, ' - Raiz unica')
 else:
-    b = float(input('Digite o valor de b(x): '))
-    c = float(input('Digite o valor de c: '))
-
-delta = (b ** 2) - (4 * a * c)
-raiz_1 = ((-(b)) + (delta) ** 0.5) / (2 * a)
-raiz_2 = ((-(b)) - ((delta) ** 0.5)) / (2 * a)
-raiz_3 = (raiz_1 == raiz_2)
-
-print('O valor de deltá é {:.2f}'.format(delta)) # 0.000000	{:.2f}	0.00	Format float 2 decimal places
-
-if (delta == 0):
-    print('A equação possui apenas uma raíz real, sendo ela = {}' .format(raiz_1))
-elif (delta >= 1):
-    print('A equação possui duas raízes reais, sendo elas: {} e {}'.format(raiz_1,raiz_2))
-else:
-    print ('A equação não possui raízes')
+    x1 = (-b + (delta ** 0.5)) / (2 * a)
+    x2 = (-b - (delta ** 0.5)) / (2 * a)
+    print('O resultado da equação é: +', x1, 'e', x2)
