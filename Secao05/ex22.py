8926f44f98f39b56f321db461c61b6744f9ffcd2
Leandro Afonso da Silva de Almeida <79644610+LeandroASAlmeida@users.noreply.github.com>
2022-04-16 15:34:04 -0300

    Update ex22.py

--- Secao05/ex22.py
+++ Secao05/ex22.py
@@ -1,24 +1,14 @@
-nome = input('Informe seu nome: ')--
-idade =int(input('Olá {}, agora informe sua idade: '.format(nome)))
-tempo= int(input('Informe o seu tempo de serviço: '))
+idade = float(input('Digite a idade do trabalhador: '))
+tempo = float(input('Digite o tempo de serviço: '))
 
-if (tempo >= 30):
-    print("Você pode se aposentar")
-elif (tempo < 30):
-    print("Você não pode se aposentar")
- 
-if (idade >= 65):
-    print("Você poderá se aposentar devido a idade!")
-elif (idade < 65):
-    print("Você não pode se aposentar somente por idade")
+if idade > 65:
+    print('Pode se aposentar!')
 
-if (tempo >= 30):
-    print("Tempo de carteira assinada OK, portanto pode se aposentar")
-elif (tempo < 30):
-    print("Sem tempo de carteira assinada sulficiente, portanto não pode se aposentar")
+elif tempo > 30:
+    print('Pode se aposentar!')
 
-if (idade >= 65):
-    print("Parabéns, você poderá se aposentar devido a idade!")
-elif (idade < 65):
-    print("Você não pode se aposentar somente por idade")
-  
+elif idade > 60 and tempo > 25:
+    print('Pode se aposentar!')
+
+else:
+    print('Não pode se aposentar!')
