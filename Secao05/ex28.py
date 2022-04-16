b59986603e859ec7d2fec2710b8b5b0ba928638e
Leandro Afonso da Silva de Almeida <79644610+LeandroASAlmeida@users.noreply.github.com>
2022-04-16 15:35:56 -0300

    Update ex28.py

--- Secao05/ex28.py
+++ Secao05/ex28.py
@@ -1,3 +1,20 @@
-num1 = int(input('Informe um ano: '))---
-num2 = int(input('Informe um ano: '))
-num3 = int(input('Informe um ano: '))
+x = float(input('Digite o valor de X: '))
+y = float(input('Digite o valor de Y: '))
+z = float(input('Digite o valor de Z: '))
+media = float(input('Digite um valor para o tipo de média: \n'
+                    '1 - Média Geometrica \n'
+                    '2 - Média Ponderada \n'
+                    '3 - Média Harmônica \n'
+                    '4 Média Aritmética \n'))
+if media == 1:
+    media = (x * y * z) ** (1/3)
+    print(f'A média Geometrica é: {media}')
+elif media == 2:
+    media = (x + 2 * y + 3 * z) / 6
+    print(f'A media ponderada é: {media}')
+elif media == 3:
+    media = (1 / x) + (1 / y) + (1 / z) / 1
+    print(f'A media harmônica é: {media}')
+elif media == 4:
+    media = (x + y + z) / 3
+    print(f'A media aritmetica é: {media}')
