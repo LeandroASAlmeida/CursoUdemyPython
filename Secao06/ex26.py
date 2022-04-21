2e78e4f9afb99dda3f0a1dbc1e776e3b3d979f45
Leandro Afonso da Silva de Almeida <79644610+LeandroASAlmeida@users.noreply.github.com>
2022-04-21 16:01:05 -0300

    Update and rename ex26.----py to ex26.py

---
+++ Secao06/ex26.py
@@ -0,0 +1,27 @@
+'''
+Faca um algoritmo que encontre o primeiro multiplo de 11, 13 ou 17 apos um numero 
+dado
+'''
+
+'''num=int(input('Digite o numero: '))'''
+
+
+num = int(input('Digite um número inteiro: '))
+
+while True:
+    num += 1
+    if (num % 11 == 0) or (num % 13 == 0) or (num % 17 == 0):
+        print(num)
+        break
+
+
+
+
+
+
+
+
+
+
+
+
