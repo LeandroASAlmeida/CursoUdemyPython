fd9cdbabfa1af13a684558dbff4d25f70a8c48fd
Leandro Afonso da Silva de Almeida <leandroslv125@gmail.com>
2022-04-23 09:27:02 -0300

    upload

--- Secao06/ex46.py
+++ Secao06/ex46.py
@@ -5,3 +5,25 @@ acerta o numero gerado. O programa deve informar em quantas tentativas o numero 
 descoberto.
 
 '''
+
+from random import randint
+
+numero = randint(1,1000) # 1 a 1000
+cont = 0
+acertou = False
+
+while not acertou: #enquanto não acertar o chute faça o loop abaixo.
+    print('JOGO DO NUMERO ALEATÓRIO'.center(40,'-'))
+    print('Tente Acertar !!!'.center(40,'-'))
+    chute= int(input('Chute um numero de 1 a 1000!\n'))
+    cont += 1
+    if chute == numero:
+        acertou = True
+    else:
+        if chute < numero:
+            print(' Errou.. é mais')
+        elif chute > numero:
+            print('Errou... é menos')
+
+print('Acertou!! o numero escolhido era {}, e você tentou {} vezes'.format(numero,cont))
+    
